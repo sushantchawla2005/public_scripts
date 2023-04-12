bc48bde3a5f0381f01af26fc4e02e10dc8f81a41
Sushant Chawla <64462127+sushantchawla2005@users.noreply.github.com>
2023-04-12 20:52:13 +0530

    Create enable-xmlrpc-protection.sh
    Added new script

---
+++ enable-xmlrpc-protection.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf ./list.txt
+
+for A in $(ls -l /home/master/applications/| grep "^d" | awk '{print $NF}'); do echo $A && if [[ -f /home/master/applications/$A/public_html/wp-config.php ]]; then echo $A >> ./list.txt; fi;done
+
+while read line
+do
+	grep xmlrpc /etc/nginx/sites-enabled/$line >> /dev/null 2>&1
+	if [[ $? -ne 0 ]]
+	then
+		echo "Adding XMLRPC to $line"
+		sed -i '/additional_server_conf/a include /etc/nginx/extras/xmlrpc_blocked.conf;' /etc/nginx/sites-enabled/$line
+	fi
+
+done < ./list.txt
