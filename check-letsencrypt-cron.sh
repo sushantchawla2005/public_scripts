2c5855bafe1d0db3be5a1239f0626ca7248619dc
Sushant Chawla <64462127+sushantchawla2005@users.noreply.github.com>
2024-10-28 09:58:22 +0530

    Update check-letsencrypt-cron.sh

--- check-letsencrypt-cron.sh
+++ check-letsencrypt-cron.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-for A in $(ls -l /home/master/applications/| grep "^d" | awk '{print $NF}'); do sudo crontab -l | grep $A > /dev/null 2>&1 || echo "No SSL auto renewal cron found for $A" && cat /home/master/applications/$A/conf/server.nginx && echo ""; done
+for A in $(ls -l /home/master/applications/ | grep "^d" | awk '{print $NF}'); do
+  if ! sudo crontab -l | grep $A > /dev/null 2>&1; then
+    echo "No SSL auto renewal cron found for $A"
+    cat /home/master/applications/$A/conf/server.nginx
+  fi
+done
