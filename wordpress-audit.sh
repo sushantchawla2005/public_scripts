8c2bb9b76143f9d92d2c85961c4f80e4bd45a6ad
Sushant Chawla <64462127+sushantchawla2005@users.noreply.github.com>
2024-11-06 14:34:26 +0530

    Update wordpress-audit.sh
    Made some changes to make the script more intelligent

--- wordpress-audit.sh
+++ wordpress-audit.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+# Author: Sushant Chawla
+# Description: Script to quickly audit a wordpress site
 ###################
 # Global Variables
 ###################
 
-WP="/usr/local/bin/wp --allow-root --skip-themes --skip-plugins"
-PREFIX=`grep "table_prefix" ./wp-config.php | awk '{print $3}'| tr -d \'\;`
+# Initialize WP-CLI
+if [ "$(id -u)" -eq 0 ]; then
+    WP="/usr/local/bin/wp --allow-root --skip-themes --skip-plugins"
+else
+    WP="/usr/local/bin/wp --skip-themes --skip-plugins"
+fi
+
+# Initialize Wordpress DB table prefix
+if [ -f ./wp-config.php ]; then
+    PREFIX=$(grep "table_prefix" ./wp-config.php | awk '{print $3}' | tr -d \'\;)
+else
+    PREFIX=$(wp db prefix)
+fi
+
 GREP="/bin/grep"
 WC="/usr/bin/wc"
 MYSQL="/usr/bin/mysql"
@@ -34,13 +48,19 @@ Cron Status: `${WP} cron event list | ${GREP} -v "hook" | ${WC} -l`"
 
 # Check transient variable
 echo -e "
-Transient varibles: `${WP} option list | ${GREP} "_site\|_trans" | wc -l`"
+Transient varibles: `${WP} transient list | wc -l`"
 
 # Check memory usage
 echo -e "
 Memory Usage `${WP} eval "echo round( memory_get_usage() / 1048576, 2 );"` MB"
 
-# Check MySQL query status
+
+# Check Autoloaded options size
 echo -e "
-MySQL queries running on the server"
-${MYSQL} -uroot -e "show processlist;"
+Autoloaded options size"
+${WP} db query "SELECT 'autoloaded data in KiB' as name, ROUND(SUM(LENGTH(option_value))/ 1024) as value FROM ${PREFIX}options WHERE autoload='yes' UNION SELECT 'autoloaded data count', count(*) FROM ${PREFIX}options WHERE autoload='yes' UNION (SELECT option_name, length(option_value) FROM ${PREFIX}options WHERE autoload='yes' ORDER BY length(option_value) DESC LIMIT 10);"
+
+# Report slow plugins
+echo -e "
+Slow Plugins List:"
+curl -s https://raw.githubusercontent.com/aphraz/cloudways/master/plugin-perf.sh | bash
