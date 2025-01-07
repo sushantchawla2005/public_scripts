2a221a4b07b3379fc26fc77e5e7221627f7dbb0f
Sushant Chawla <64462127+sushantchawla2005@users.noreply.github.com>
2025-01-07 18:09:03 +0530

    Create collect-stats.sh script

---
+++ collect-stats.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DATE=$(/bin/date +%F--%H-%M)
+# Define output file
+OUTPUT_FILE="/root/innodb_lock_debug.log"
+
+
+# Clear previous log content
+echo "Extracting useful information from server..." > "$OUTPUT_FILE"
+
+echo "== ${DATE} ==" >> ${OUTPUT_FILE}
+
+# Run the SHOW ENGINE INNODB STATUS command
+mysql -e "SHOW ENGINE INNODB STATUS\G;" >> ${OUTPUT_FILE}
+
+# Run the processlist command
+mysql -e "show processlist;" >> ${OUTPUT_FILE}
+
+# Run iostat
+iostat -x >> ${OUTPUT_FILE}
+
+# Notify user
+echo "Debugging information has been saved to $OUTPUT_FILE"
