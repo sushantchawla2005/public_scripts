c0aa2cce01cd68e41ece8cb24a7993aba61e98a5
Sushant Chawla <64462127+sushantchawla2005@users.noreply.github.com>
2025-01-07 18:11:39 +0530

    Update collect-stats.sh

--- collect-stats.sh
+++ collect-stats.sh
@@ -2,8 +2,7 @@
 
 DATE=$(/bin/date +%F--%H-%M)
 # Define output file
-OUTPUT_FILE="/root/innodb_lock_debug.log"
-
+OUTPUT_FILE="/root/server-stats.log"
 
 # Clear previous log content
 echo "Extracting useful information from server..." > "$OUTPUT_FILE"
@@ -14,9 +13,11 @@ echo "== ${DATE} ==" >> ${OUTPUT_FILE}
 mysql -e "SHOW ENGINE INNODB STATUS\G;" >> ${OUTPUT_FILE}
 
 # Run the processlist command
+echo -e "######## PROCESS LIST ########" >> ${OUTPUT_FILE}
 mysql -e "show processlist;" >> ${OUTPUT_FILE}
 
 # Run iostat
+echo -e "######## DISK STATUS ########" >> ${OUTPUT_FILE}
 iostat -x >> ${OUTPUT_FILE}
 
 # Notify user
